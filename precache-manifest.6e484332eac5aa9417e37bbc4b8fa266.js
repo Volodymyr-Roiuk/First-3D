ee3079d7728afc9865c8ad521adc481ef0ebbe38
Volodymyr_Roiuk <heppivova@gmail.com>
2020-02-09 23:01:14 +0300

    Updates

---
+++ precache-manifest.6e484332eac5aa9417e37bbc4b8fa266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04345abc1938265377af952d9da6521b",
+    "url": "/First-3D/index.html"
+  },
+  {
+    "revision": "6040ecf9d0e622f17c0d",
+    "url": "/First-3D/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f663162b0dbdd77f6aeb",
+    "url": "/First-3D/static/js/2.2b4c03e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/First-3D/static/js/2.2b4c03e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6040ecf9d0e622f17c0d",
+    "url": "/First-3D/static/js/main.6415d493.chunk.js"
+  },
+  {
+    "revision": "11dc97e25899bff4a9e6",
+    "url": "/First-3D/static/js/runtime-main.4bdfde36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-3D/static/media/logo.5d5d9eef.svg"
+  }
+]);
